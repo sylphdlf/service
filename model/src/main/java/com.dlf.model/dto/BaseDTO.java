f1dd329936da484db7d58e3480dfc699ac4aca88
Someone <oncwnuOIzH_4tT_FuTFpX7g3Kj3U@git.weixin.qq.com>
2020-04-20 18:51:01 +0800

    20200420

--- model/src/main/java/com.dlf.model/dto/BaseDTO.java
+++
@@ -1,7 +0,0 @@
-package com.dlf.model.dto;
-
-import lombok.Data;
-@Data
-public class BaseDTO {
-
-}
---
+++ model/src/main/java/com.dlf.model/dto/user/WxUserReqDTO.java
@@ -0,0 +1,15 @@
+package com.dlf.model.dto.user;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class WxUserReqDTO {
+
+    private String openId;
+
+    private String mobile;
+
+    private String verifyCode;
+}
