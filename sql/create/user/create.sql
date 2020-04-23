fc1866803cc7b98f8a01c83dc5c7df05f52ba3bb
sylphdlf@163.com <dlf@1989128>
2020-04-23 22:43:40 +0800

    20200423

--- sql/create/user/create.sql
+++ sql/create/user/create.sql
@@ -94,9 +94,7 @@ CREATE TABLE IF NOT EXISTS `u_org_role` (
 
 CREATE TABLE IF NOT EXISTS `u_role` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `org_id` BIGINT(20) NOT NULL COMMENT '机构id',
-  `code` VARCHAR(60) DEFAULT NULL COMMENT '角色编码,方便业务系统赋权限',
-  `name` VARCHAR(128) NOT NULL COMMENT '角色名称',
+  `name` VARCHAR(50) NOT NULL COMMENT '角色名称',
   `remarks` VARCHAR(255) DEFAULT NULL COMMENT '备注',
   `create_user_id` BIGINT(20) NULL COMMENT '创建人',
   `create_time` TIMESTAMP DEFAULT current_timestamp COMMENT '创建时间',
@@ -104,8 +102,7 @@ CREATE TABLE IF NOT EXISTS `u_role` (
   `update_time` TIMESTAMP NULL DEFAULT NULL COMMENT '最后修改时间',
   `org_code` VARCHAR(32) NULL COMMENT '组织机构代码',
   `is_deleted` INT(2) DEFAULT '0' COMMENT '是否逻辑删除:默认0未删除,1已删除',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_code` (`code`)
+  PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='角色表';
 
 CREATE TABLE IF NOT EXISTS `u_role_fun` (
