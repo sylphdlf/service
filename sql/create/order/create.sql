656d4ee156493bd0ba67644e5eb3334da44faced
sylphdlf@163.com <dlf@1989128>
2020-05-12 23:09:17 +0800

    ～～～

--- sql/create/order/create.sql
+++ sql/create/order/create.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS o_order (
     update_time TIMESTAMP NULL DEFAULT NULL COMMENT '最后修改时间',
     org_code VARCHAR(32) NULL COMMENT '组织机构代码',
     is_deleted INT(2) DEFAULT '0' COMMENT '是否逻辑删除:默认0未删除,1已删除',
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (id)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='订单表';
 
 CREATE TABLE IF NOT EXISTS o_order_sub (
@@ -18,7 +18,6 @@ CREATE TABLE IF NOT EXISTS o_order_sub (
     code varchar(23) not null comment '子单编号',
     product_id bigint(20) not null comment '商品id',
     product_num int(5) not null default 1 comment '商品数量',
-    user_file_id varchar(500) null comment '关联文件id,f_user_file id',
     status INT(2) DEFAULT '0' COMMENT '状态',
     remarks VARCHAR(255) DEFAULT NULL COMMENT '备注',
     create_user_id BIGINT(20) NULL COMMENT '创建人',
@@ -27,5 +26,20 @@ CREATE TABLE IF NOT EXISTS o_order_sub (
     update_time TIMESTAMP NULL DEFAULT NULL COMMENT '最后修改时间',
     org_code VARCHAR(32) NULL COMMENT '组织机构代码',
     is_deleted INT(2) DEFAULT '0' COMMENT '是否逻辑删除:默认0未删除,1已删除',
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (id)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='订单表';
+
+CREATE TABLE IF NOT EXISTS o_order_file (
+    id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    order_id bigint(20) not null comment '子单id',
+    file_id varchar(200) not null comment '文件id集合，f_user_file表id',
+    status INT(2) DEFAULT '0' COMMENT '状态',
+    remarks VARCHAR(255) DEFAULT NULL COMMENT '备注',
+    create_user_id BIGINT(20) NULL COMMENT '创建人',
+    create_time TIMESTAMP DEFAULT current_timestamp COMMENT '创建时间',
+    update_user_id BIGINT(20) NULL COMMENT '最后修改人',
+    update_time TIMESTAMP NULL DEFAULT NULL COMMENT '最后修改时间',
+    org_code VARCHAR(32) NULL COMMENT '组织机构代码',
+    is_deleted INT(2) DEFAULT '0' COMMENT '是否逻辑删除:默认0未删除,1已删除',
+    PRIMARY KEY (id)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='订单表';
